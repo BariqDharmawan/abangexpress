bfcaf1b0863600a738b816e06a7c7145279a7379
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2021-09-30 16:36:02 +0700

    [FIX] data not found

--- resources/views/admin/client/form.blade.php
+++ resources/views/admin/client/form.blade.php
@@ -1,26 +1,26 @@
-@if ($data)
+@isset ($data)
     <img src="{{ $data->logo }}" class="card-img-top mb-5" alt="">
-@endif
+@endisset
 <form method="POST" enctype="multipart/form-data" action="{{ $action }}">
     @isset($data)
         @csrf @method('PUT')
     @endisset
     <div class="form-group">
-        <label for="@if($data) client-name-{{ $data->id }} @else client-title @endif">Client Fullname</label>
+        <label for="@isset($data) client-name-{{ $data->id }} @else client-title @endisset">Client Fullname</label>
         <input type="text" class="form-control"
-        id="@if($data) client-name-{{ $data->id }} @else client-title @endif" name="name" 
-        value="@if($data){{ $data->name }}@endif" required>
+        id="@isset($data) client-name-{{ $data->id }} @else client-title @endisset" name="name" 
+        value="@isset($data){{ $data->name }}@endisset" required>
     </div>
     <div class="form-group">
         <div class="custom-file">
             <input type="file" class="custom-file-input" 
             id="client-logo" name="logo" required>
             <label class="custom-file-label" for="client-logo">
-                @if($data)
+                @isset($data)
                 Change logo
                 @else
                 Choose logo
-                @endif
+                @endisset
             </label>
         </div>
     </div>
--- resources/views/admin/services/form.blade.php
+++ resources/views/admin/services/form.blade.php
@@ -1,32 +1,32 @@
-@if ($data)
+@isset ($data)
     <img src="{{ asset($data->icon) }}" alt="" height="30px" class="d-block mb-3 mx-auto">
-@endif
+@endisset
 <form method="POST" enctype="multipart/form-data" action="{{ $action }}">
     @isset($data)
         @csrf @method('PUT')
     @endisset
     <div class="form-group">
-        <label for="@if($data) service-title-{{ $data->id }} @else service-title @endif">Service name</label>
+        <label for="@isset($data) service-title-{{ $data->id }} @else service-title @endisset">Service name</label>
         <input type="text" class="form-control" 
-        id="@if($data) service-title-{{ $data->id }} @else service-title @endif" name="title" 
-        value="@if($data){{ $data->title }}@endif" required>
+        id="@isset($data) service-title-{{ $data->id }} @else service-title @endisset" name="title" 
+        value="@isset($data){{ $data->title }}@endisset" required>
     </div>
     <div class="form-group">
-        <label for="@if($data) service-desc-{{ $data->id }} @else service-desc @endif">Service desc</label>
-        <textarea name="desc" id="@if($data)service-desc-{{ $data->id }} @else service-desc @endif" rows="3" 
+        <label for="@isset($data) service-desc-{{ $data->id }} @else service-desc @endisset">Service desc</label>
+        <textarea name="desc" id="@isset($data)service-desc-{{ $data->id }} @else service-desc @endisset" rows="3" 
         style="resize: none;" 
-        class="form-control" required>@if($data){{ $data->desc }}@endif</textarea>
+        class="form-control" required>@isset($data){{ $data->desc }}@endisset</textarea>
     </div>
     <div class="form-group">
         <div class="custom-file">
             <input type="file" class="custom-file-input" 
             id="service-icon" name="icon" required>
             <label class="custom-file-label" for="service-icon">
-                @if($data)
+                @isset($data)
                 Change icon
                 @else
                 Choose icon
-                @endif
+                @endisset
             </label>
         </div>
     </div>
