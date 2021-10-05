92c08d57c2d5dc743c92a2e75880b47a5844f388
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2021-10-05 19:13:46 +0700

    [UPDATE] readme and remove unused JS

--- resources/assets/admin/js/vendor.js
+++
@@ -1,8 +0,0 @@
-// window.$ = window.jQuery = require('jquery');
-// require('bootstrap');
-// window.Popper = require('popper.js').default;
-// require('jquery.easing')(window.jQuery);
-
-// import './../template/vendor/jquery-easing/jquery.easing.min.js'
-// import './../template/js/sb-admin-2.min.js'
-// import './../template/vendor/bootstrap/js/bootstrap.bundle.min.js'
