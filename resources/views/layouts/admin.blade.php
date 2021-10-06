bf61a91ed053e28582ba7dd4d52f4117ae5c4dad
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2021-10-06 11:17:46 +0700

    [UPDATE] remove unused view

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -81,45 +81,54 @@
 
         <div id="content-wrapper" class="d-flex flex-column">
             <div id="content">
-                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 
-                static-top shadow">
+                <nav class="navbar navbar-expand navbar-light 
+                bg-white topbar mb-4 static-top shadow">
                     <!-- Sidebar Toggle (Topbar) -->
-                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                    <button id="sidebarToggleTop" 
+                    class="btn btn-link d-md-none rounded-circle mr-3">
                         <i class="fa fa-bars"></i>
                     </button>
-                    <!-- Topbar Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <div class="topbar-divider d-none d-sm-block"></div>
-                        <!-- Nav Item - User Information -->
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ auth()->user()->username }}</span>
-                                <img class="img-profile rounded-circle" src="{{ asset('admin/img/undraw_profile.svg') }}">
+                                <span 
+                                class="mr-2 d-none d-lg-inline text-gray-600 small">
+                                    {{ auth()->user()->username }}
+                                </span>
+                                <img class="img-profile rounded-circle" 
+                                src="{{ asset('admin/img/undraw_profile.svg') }}">
                             </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                                @if (!Str::contains(url()->current(), 'shipment'))
+                                <a class="dropdown-item" href="/shipment">
+                                    Shipment
+                                </a>
+                                @else
+                                <a class="dropdown-item" href="/shipment">
+                                    Company Profile
+                                </a>
+                                @endif
                                 <form action="{{ route('logout') }}" method="POST">
                                     @csrf
-                                    <a class="dropdown-item" href="javascript:void(0);" 
-                                    onclick="event.preventDefault();
-                                    this.closest('form').submit();">
+                                    <button class="dropdown-item" type="submit">
                                         <i class="fas fa-sign-out-alt 
-                                        fa-sm fa-fw mr-2 text-gray-400"></i>
+                                        fa-sm fa-fw mr-2"></i>
                                         Logout
-                                    </a>
+                                    </button>
                                 </form>
                             </div>
                         </li>
                     </ul>
                 </nav>
-                <!-- End of Topbar -->
                 @yield('content')
             </div>
             @include('admin.partials.footer')
         </div>
-
         @include('admin.partials.btn-back-to-top')
     </div>
+
     <script src="{{ asset('admin/template/vendor/jquery/jquery.min.js') }}"></script>
     <script src="{{ asset('admin/js/vendor.js') }}"></script>
     <script src="{{ asset('admin/js/app.js') }}"></script>
