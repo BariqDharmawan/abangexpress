d37a51a1418d090750932072ec3c572a2180f5ac
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2021-10-06 18:32:53 +0700

    [UPDATE] validation backend and JS

--- app/Rules/AlphaSpaceRule.php
+++ app/Rules/AlphaSpaceRule.php
@@ -25,7 +25,7 @@ class AlphaSpaceRule implements Rule
      */
     public function passes($attribute, $value)
     {
-        return preg_match('/^[a-zA-Z\s]*$/', $value);
+        return preg_match('/^[a-zA-Z.\s]*$/', $value);
     }
 
     /**
--- resources/assets/admin/js/component/input.js
+++ resources/assets/admin/js/component/input.js
@@ -4,14 +4,14 @@ $(document).ready(function () {
     bsCustomFileInput.init()
 
     $(".not-allow-number").on('keydown', function (e) {
-        if (e.shiftKey || e.ctrlKey || e.altKey) {
+        if (e.ctrlKey || e.altKey) {
             e.preventDefault()
         } else {
             const key = e.keyCode
     
-            if (!((key == 8) || (key == 32) || (key == 46) || (key >= 35 && key <= 40) || 
-            (key >= 65 && key <= 90))) {
-                e.preventDefault();
+            if (!((key == 8) || (key == 32) || (key == 46) || (key >= 35 && key <= 40) 
+            || (key >= 65 && key <= 90) || key == 190)) {
+                e.preventDefault()
             }
         }
     })
