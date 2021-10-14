57a8a2a2c965eb6542d1acdf9e83f165ae8c0ce0
bencol3nk <81988206+bencol3nk@users.noreply.github.com>
2021-10-15 04:09:58 +0700

    [UPDATE] update delete commercial invoice items
    done 99% validation check hasnt been occured

--- resources/assets/shipment/js/localstorage.js
+++ resources/assets/shipment/js/localstorage.js
@@ -225,98 +225,139 @@ $(document).ready(function() {
 
     })
 
+    function FillCommercialInvoice() {
+        // generate commercial invoice table data
+        var oldCi = localStorage.getItem("commercialInvoice")
+        if (oldCi === null) {
+            // no data dont generate
 
-    // generate commercial invoice table data
-    var oldCi = localStorage.getItem("commercialInvoice")
-    if (oldCi === null) {
-        // no data dont generate
-    } else {
-        // generate data
+            $("#btn-generate-pdf").attr("disabled");
+        } else {
+            // generate data
 
-        const thisForm = $(this)[0]
-        const btnSubmit = $(this).find("button[type='submit']")
+            const thisForm = $(this)[0]
+            const btnSubmit = $(this).find("button[type='submit']")
 
 
-        // refresh dataTable
-        const data = JSON.parse(
-            '[' + localStorage.getItem("commercialInvoice") + ']'
-        )
-        const commercialInvoice = data.map((invoice, index) => ({
-            ...invoice,
-            no: index + 1,
-            total_value: Number(invoice.quantity) *
-                Number(invoice.value_unit),
-            action: `<button class="btn waves-effect btn-danger"
-                        data-toggle="modal" type="button"
-                        data-target="#delete-data-${index + 1}">
-                            <i class="material-icons">delete</i>
-                        </button>`
-        }))
+            // refresh dataTable
+            const data = JSON.parse(
+                '[' + localStorage.getItem("commercialInvoice") + ']'
+            )
+            const commercialInvoice = data.map((invoice, index) => ({
+                ...invoice,
+                no: index + 1,
+                total_value: Number(invoice.quantity) *
+                    Number(invoice.value_unit),
+                action: `<button class="btn waves-effect btn-danger"
+                            data-toggle="modal" type="button"
+                            data-target="#delete-data-${index + 1}">
+                                <i class="material-icons">delete</i>
+                            </button>`
+            }))
 
-        console.log('data', commercialInvoice)
-        $('#commercialInvoice').DataTable().destroy()
-        $('#commercialInvoice').DataTable({
-            "data": commercialInvoice,
-            "columns": [
-                { "data": "no" },
-                { "data": "desc" },
-                { "data": "unit" },
-                { "data": "quantity" },
-                { "data": "value_unit" },
-                { "data": "total_value" },
-                { "data": "action" }
-            ]
-        })
-        $('#commercialInvoice').DataTable().draw()
-
-        $("#btn-generate-pdf").removeAttr("disabled");
-
-        for (let i = 0; i < commercialInvoice.length; i++) {
-            const modalFade = $("<div>", {
-                id: `delete-data-${i + 1}`,
-                class: 'modal fade',
-                tabindex: '-1',
-                role: 'dialog'
+            console.log('data', commercialInvoice)
+            $('#commercialInvoice').DataTable().destroy()
+            $('#commercialInvoice').DataTable({
+                "data": commercialInvoice,
+                "columns": [
+                    { "data": "no" },
+                    { "data": "desc" },
+                    { "data": "unit" },
+                    { "data": "quantity" },
+                    { "data": "value_unit" },
+                    { "data": "total_value" },
+                    { "data": "action" }
+                ]
             })
+            $('#commercialInvoice').DataTable().draw()
 
-            const csrfToken = $("meta[name='csrf-token']").attr('content')
-            $(`<div class="modal fade" id="delete-data-${i + 1}"
-                        tabindex="-1" role="dialog">
-                            <div class="modal-dialog" role="document">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h4 class="modal-title" id="{{ $id }}Label">
-                                            Hapus data ${i + 1}
-                                        </h4>
-                                    </div>
-                                    <div class="modal-body">
-                                        <p>
-                                            Apakah kamu yakin, ingin menghapus data
-                                            ${i + 1}
-                                        </p>
-                                    </div>
-                                    <div class="modal-footer">
-                                        <div class="d-flex justify-content-between">
-                                            <button type="button" class="btn btn-primary waves-effect" data-dismiss="modal">
-                                                Tidak jadi
-                                            </button>
-                                            <form id="form-remove-invoice" action="" method="POST">
-                                                <input type="hidden" name="_token"
-                                                value="${csrfToken}">
-                                                <input type="hidden" name="_method"
-                                                value="DELETE">
-                                                <button type="button" class="btn btn-danger waves-effect">
-                                                    Ya, hapus
+            $("#btn-generate-pdf").removeAttr("disabled");
+
+            for (let i = 0; i < commercialInvoice.length; i++) {
+                const modalFade = $("<div>", {
+                    id: `delete-data-${i + 1}`,
+                    class: 'modal fade',
+                    tabindex: '-1',
+                    role: 'dialog'
+                })
+
+                const csrfToken = $("meta[name='csrf-token']").attr('content')
+                $(`
+
+                <div class="modal fade" id="delete-data-${i + 1}"
+                            tabindex="-1" role="dialog">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h4 class="modal-title" id="{{ $id }}Label">
+                                                Hapus data ${i + 1}
+                                            </h4>
+                                        </div>
+                                        <div class="modal-body">
+                                            <p>
+                                                Apakah kamu yakin, ingin menghapus data
+                                                ${i + 1}
+                                            </p>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <div class="d-flex justify-content-between">
+                                                <button type="button" class="btn btn-primary waves-effect" data-dismiss="modal">
+                                                    Tidak jadi
                                                 </button>
-                                            </form>
+                                                    <input type="hidden" name="_token"
+                                                    value="${csrfToken}">
+                                                    <input type="hidden" name="_method"
+                                                    value="DELETE">
+                                                    <button id="xbutton" value="${i + 1}" type="button"data-dismiss="modal"  class="btn btn-danger waves-effect">
+                                                        Ya, hapus
+                                                    </button>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    `).appendTo('body')
+                        `).appendTo('body')
+            }
+
         }
 
     }
+
+
+    FillCommercialInvoice()
+        // delete commercial invoice content
+    $("button").click(function() {
+        var ids = $(this).val();
+        if (ids > 0) {
+            let data = localStorage.getItem("commercialInvoice");
+            // split and count
+            const xArray = data.split("},");
+            if (xArray.length > 0) {
+
+                var removeId = ids - 1;
+                var str1 = "";
+                var ct = 1;
+                for (let i = 0; i < xArray.length; i++) {
+                    if (i == ids - 1) {} else {
+                        var xstr2 = "";
+                        xstr2 = xArray[i].replace("{", "")
+                        xstr2 = xstr2.replace("}", "")
+                        if (ct == 1) {
+                            str1 = str1.concat('{' + xstr2 + '}')
+                        } else {
+                            str1 = str1.concat(',{' + xstr2 + '}')
+                        }
+                        ct++
+                    }
+                }
+                localStorage.setItem('commercialInvoice', str1)
+                console.log(str1)
+                FillCommercialInvoice()
+            } else {
+                // remove key if only 1 found
+                localStorage.removeItem('commercialInvoice')
+            }
+        }
+    });
 
 })
