4220a5009166d0da31b6ad2c3ef127b447e2e5f8
bencol3nk <81988206+bencol3nk@users.noreply.github.com>
2021-10-15 07:57:21 +0700

    [UPDATE] updating data table view  on sub menu order
    adding header on all data table view on sub menu order

--- resources/views/shipment/order/index.blade.php
+++ resources/views/shipment/order/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.shipment')
 
-@section('title', $title)
+{{-- @section('title', $title) --}}
 
 @section('content')
 
@@ -8,6 +8,11 @@
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12"
     id="parent-{{ $tableClass}}">
         <div class="card">
+            <div class="header">
+                <h2>
+                    {{$title}}
+                </h2>
+            </div>
             <div class="body">
                 <div class="table-responsive">
                     <table id="{{ $tableClass}}" class="table table-bordered table-striped table-hover js-basic-example dataTable w-100">
