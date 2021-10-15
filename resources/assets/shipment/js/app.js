59e52f105b19a1eb3f5e5187bbfb17cc780aa023
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2021-10-15 19:16:37 +0700

    [UPDATE] preview PDF #2

--- resources/assets/shipment/js/app.js
+++ resources/assets/shipment/js/app.js
@@ -4,3 +4,15 @@ import './../template/js/demo'
 import './get-ajax'
 import './input'
 import './localstorage'
+import PDFObject from 'pdfobject'
+
+$(".preview-pdf--without-header").each(function () {
+    const pdfPreviewEl = $(this).attr('id')
+    const pdfSrc = $(this).data('pdf-src')
+
+    PDFObject.embed(pdfSrc, `#${pdfPreviewEl}`, {
+        fallbackLink: `<p>Browser device mu tidak support embed PDF, 
+                            <a href='[url]'>download saja</a>
+                        </p>`
+    })
+})
--- resources/views/shipment/support/guide.blade.php
+++ resources/views/shipment/support/guide.blade.php
@@ -1,12 +1,17 @@
 @extends('layouts/shipment')
 
-@section('title', $title)
-
 @section('content')
-    <div class="card">
-        <div class="body p-0">
-            <iframe src="{{ Storage::url('files/dummies.pdf') }}" 
-            frameborder="0" class="preview-pdf preview-pdf--full"></iframe>
+<div class="row clearfix">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div class="card">
+            <div class="header">
+                <h2>{{ $title }}</h2>
+            </div>
+            {{-- buat component filter dsbnya --}}
+            <div class="header"></div>
+            <div class="body text-center">
+                <div class="preview-pdf preview-pdf--full preview-pdf--without-header" id="pdf-guide" data-pdf-src="{{ Storage::url('files/dummies.pdf') }}"></div>
+            </div>
         </div>
     </div>
 @endsection
