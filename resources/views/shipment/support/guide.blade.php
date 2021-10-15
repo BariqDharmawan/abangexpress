36db9bcb454cac59683bda6af17aa3ec6bbf362e
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2021-10-15 18:50:09 +0700

    [ADD] preview pdf

--- resources/views/shipment/support/guide.blade.php
+++ resources/views/shipment/support/guide.blade.php
@@ -3,5 +3,10 @@
 @section('title', $title)
 
 @section('content')
-    
+    <div class="card">
+        <div class="body p-0">
+            <iframe src="{{ Storage::url('files/dummies.pdf') }}" 
+            frameborder="0" class="preview-pdf preview-pdf--full"></iframe>
+        </div>
+    </div>
 @endsection
