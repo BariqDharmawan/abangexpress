34dcc2564fdec32face9cbb1a233878202d91266
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2021-10-15 19:28:29 +0700

    [UPDATE] card page

--- resources/views/shipment/index.blade.php
+++ resources/views/shipment/index.blade.php
@@ -16,17 +16,24 @@
         </div>
     </form>
 
-    {{-- <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-        <div class="info-box bg-orange hover-expand-effect">
-            <div class="icon">
-                <i class="material-icons">person_add</i>
+    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+        <div class="card">
+            <div class="header">
+                <h2>Kiriman belum ditagih</h2>
             </div>
-            <div class="content">
-                <div class="text">NEW VISITORS</div>
-                <div class="number count-to" data-from="0" data-to="1225" data-speed="1000" data-fresh-interval="20"></div>
+            <div class="body">
+                <div class="info-box bg-orange hover-expand-effect">
+                    <div class="icon">
+                        <img src="{{ asset('img/icon/savings_black_24dp.svg') }}" alt="">
+                    </div>
+                    <div class="content">
+                        <div class="text">NEW VISITORS</div>
+                        <div class="number count-to" data-from="0" data-to="1225" data-speed="1000" data-fresh-interval="20"></div>
+                    </div>
+                </div>
             </div>
         </div>
-    </div> --}}
+    </div>
 
     @if (session('trackingstatus'))
     <section id="search-resi-section" class="mb-5">
--- resources/views/shipment/invoice/verifying.blade.php
+++ resources/views/shipment/invoice/verifying.blade.php
@@ -1,7 +1,17 @@
 @extends('layouts/shipment')
 
-@section('title', $title)
-
 @section('content')
-    
+<div class="row clearfix">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div class="card">
+            <div class="header">
+                <h2>{{ $title }}</h2>
+            </div>
+            <div class="header"></div>
+            <div class="body">
+
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
