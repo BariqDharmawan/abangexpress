6d288eb735470e82acbd205740cec0ecd324fd56
bencol3nk <81988206+bencol3nk@users.noreply.github.com>
2021-10-18 12:11:39 +0700

    [UPDATE] adding new feature print and icon change on sidebar

--- app/Http/Controllers/BookingOrderController.php
+++ app/Http/Controllers/BookingOrderController.php
@@ -359,6 +359,14 @@ class BookingOrderController extends Controller
     {
         //
     }
+    public function prints(Request $request)
+    {
+        $token=$request->link;
+
+        $halaman=file_get_contents("https://duniaexportimport.com/".$token);
+
+        echo $halaman;
+    }
 }
 
 
