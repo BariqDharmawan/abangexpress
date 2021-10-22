b766722d228bb2f3f96684cf49895a0e1ff0ca45
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2021-10-22 19:18:15 +0700

    [UPDATE] refactoring get current user key api

--- app/Http/Controllers/ShipmentInvoiceController.php
+++ app/Http/Controllers/ShipmentInvoiceController.php
@@ -5,24 +5,19 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+
 class ShipmentInvoiceController extends Controller
 {
     public function bill()
     {
         $title = 'Tagihan';
+
         // api pull data tagihan here
-        $tableClass='dataOrder';
+        $tableClass = 'dataOrder';
 
-        $uid=Auth::user()->username;
-        $user = User::where([
-            ['username', $uid]
-        ])->first();
-
-        $akun = $user->code_api;
-        $tokenkey = $user->token_api;
         $postdata = '{
-            "akun": "'.$akun.'",
-            "key": "'.$tokenkey.'",
+            "akun": "' . Auth::user()->code_api . '",
+            "key": "' . Auth::user()->token_api . '",
             "xparam":"tagihan"
         }';
 
@@ -49,27 +44,20 @@ class ShipmentInvoiceController extends Controller
         $res = json_decode($response);
         $orderData = $res->response;
         $statusRes = $res->status;
-        // dd($res);
-        // return view('shipment.order.index', compact('title','tableClass','orderData','statusRes','underling'));
-        return view('shipment.invoice.bill', compact('title','tableClass','orderData','statusRes'));
+
+        return view('shipment.invoice.bill', compact('title', 'tableClass', 'orderData', 'statusRes'));
     }
 
     public function verifying()
     {
         $title = 'Dalam Prose Verifikasi';
+
         // api pull data tagihan here
-        $tableClass='dataOrder';
+        $tableClass = 'dataOrder';
 
-        $uid=Auth::user()->username;
-        $user = User::where([
-            ['username', $uid]
-        ])->first();
-
-        $akun = $user->code_api;
-        $tokenkey = $user->token_api;
         $postdata = '{
-            "akun": "'.$akun.'",
-            "key": "'.$tokenkey.'",
+            "akun": "' . Auth::user()->code_api . '",
+            "key": "' . Auth::user()->token_api . '",
             "xparam":"vp"
         }';
 
@@ -97,52 +85,48 @@ class ShipmentInvoiceController extends Controller
         $orderData = $res->response;
         $statusRes = $res->status;
 
-        return view('shipment.invoice.verifying', compact('title','tableClass','orderData','statusRes'));
+        return view('shipment.invoice.verifying', compact('title', 'tableClass', 'orderData', 'statusRes'));
     }
 
     public function settled()
     {
         $title = 'Lunas';
-                // api pull data tagihan here
-                $tableClass='dataOrder';
+        
+        // api pull data tagihan here
+        $tableClass = 'dataOrder';
 
-                $uid=Auth::user()->username;
-                $user = User::where([
-                    ['username', $uid]
-                ])->first();
-
-                $akun = $user->code_api;
-                $tokenkey = $user->token_api;
-                $postdata = '{
-                    "akun": "'.$akun.'",
-                    "key": "'.$tokenkey.'",
+        $akun = Auth::user()->code_api;
+        $tokenkey = Auth::user()->token_api;
+        $postdata = '{
+                    "akun": "' . $akun . '",
+                    "key": "' . $tokenkey . '",
                     "xparam":"lunas"
                 }';
 
-                $curl = curl_init();
-                curl_setopt_array($curl, array(
-                    CURLOPT_URL => 'https://res.abangexpress.id/shipments/pull/datainvoice/',
-                    CURLOPT_RETURNTRANSFER => true,
-                    CURLOPT_ENCODING => '',
-                    CURLOPT_MAXREDIRS => 10,
-                    CURLOPT_TIMEOUT => 0,
-                    CURLOPT_FOLLOWLOCATION => true,
-                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                    CURLOPT_CUSTOMREQUEST => 'POST',
-                    CURLOPT_POSTFIELDS => $postdata,
-                    CURLOPT_HTTPHEADER => array(
-                        'Content-Type: application/json'
-                    ),
-                ));
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://res.abangexpress.id/shipments/pull/datainvoice/',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => $postdata,
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json'
+            ),
+        ));
 
-                $response = curl_exec($curl);
+        $response = curl_exec($curl);
 
-                curl_close($curl);
+        curl_close($curl);
 
-                $res = json_decode($response);
-                $orderData = $res->response;
-                $statusRes = $res->status;
+        $res = json_decode($response);
+        $orderData = $res->response;
+        $statusRes = $res->status;
 
-        return view('shipment.invoice.settled', compact('title','tableClass','orderData','statusRes'));
+        return view('shipment.invoice.settled', compact('title', 'tableClass', 'orderData', 'statusRes'));
     }
 }
