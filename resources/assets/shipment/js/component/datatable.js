a6e3e79de32c4db9b0c6705f30ad955bbb75aec2
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2021-10-25 14:02:41 +0700

    [UPDATE] change button export location

--- resources/assets/shipment/js/component/datatable.js
+++ resources/assets/shipment/js/component/datatable.js
@@ -6,6 +6,19 @@ $(document).ready(function () {
                 extend: 'excel',
                 text: 'Convert ke excel'
             }
-        ]
+        ],
+        initComplete: function () {
+            var $buttons = $('.dt-buttons').hide();
+            const btnExportCustom = $(
+                `.datas-action[data-datable-id="#${$(
+                    ".datatable-export-excel"
+                ).attr('id')}"]`
+            )
+
+            btnExportCustom.find('.btn-export-custom').click(function () {
+                const btnExport = $(this).data('export-type')
+                $(`.buttons-${btnExport}`).click()
+            })
+        }
     })
 })
--- resources/views/shipment/order/index.blade.php
+++ resources/views/shipment/order/index.blade.php
@@ -5,8 +5,7 @@
 @section('content')
 
 <div class="row clearfix">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12"
-    id="parent-{{ $tableClass}}">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" id="parent-{{ $tableClass}}">
         <div class="card">
             <div class="header">
                 <h2>
@@ -14,47 +13,57 @@
                 </h2>
             </div>
             <div class="header">
-                <button type="button" class="btn m-b-10 bg-green waves-effect" data-toggle="collapse" data-target="#myxDIV" aria-expanded="true" aria-controls="myxDIV">
-                    <i class="material-icons">filter_list</i>
-                    <span>Filter</span>
-                </button>
-               <div class="collapse" id="myxDIV" aria-expanded="true">
-                    <div  class="well">
+                <div class="datas-action" data-datable-id="#{{ $tableClass }}">
+                    <button type="button" class="btn m-b-10 bg-grey waves-effect" data-toggle="collapse"
+                    data-target="#filter-order" aria-expanded="true" aria-controls="filter-order">
+                        <i class="material-icons">filter_list</i>
+                        <span>Filter</span>
+                    </button>
+
+                    <button type="button" class="btn m-b-10 bg-green waves-effect btn-export-custom" data-export-type="excel">
+                        <i class="material-icons">
+                            file_download
+                        </i>
+                        <span>Excel</span>
+                    </button>
+                </div>
+                
+                <div class="collapse" id="filter-order" aria-expanded="true">
+                    <div class="well">
                         <div class="row">
 
                             <form role="form" method="POST" action="{{ route('shipping.order.filter.order') }}"
-                            autocomplete="off">
+                                autocomplete="off">
                                 @csrf
                                 <div class="col-lg-4">
-                                            <div class="form-group">
-                                                <label for="disabledSelect">Tanggal Awal</label>
-                                                <input class="form-control" name="awal" type="date">
-                                            </div>
-                                            <div class="form-group">
-                                                <label for="disabledSelect">Pengirim</label>
-                                                <input class="form-control" name="pengirim" type="text">
-                                            </div>
+                                    <div class="form-group">
+                                        <label for="disabledSelect">Tanggal Awal</label>
+                                        <input class="form-control" name="awal" type="date">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="disabledSelect">Pengirim</label>
+                                        <input class="form-control" name="pengirim" type="text">
+                                    </div>
                                 </div>
                                 <div class="col-lg-4">
 
-                                        <div class="form-group">
-                                            <label for="disabledSelect">Tanggal Akhir</label>
-                                            <input class="form-control" name="akhir" type="date">
-                                        </div>
+                                    <div class="form-group">
+                                        <label for="disabledSelect">Tanggal Akhir</label>
+                                        <input class="form-control" name="akhir" type="date">
+                                    </div>
 
-                                        <div class="form-group">
-                                            <x-shipment.input type="select"
-                                            placeholder="Sub Cabang"
-                                            name="kodeanak" required>
+                                    <div class="form-group">
+                                        <x-shipment.input type="select" placeholder="Sub Cabang" name="kodeanak"
+                                            required>
                                             @if (count($underling)>1)
-                                                @foreach ($underling as $underling)
-                                                    <option value="{{$underling->kodeAgen}}">{{$underling->nama}}</option>
-                                                @endforeach
+                                            @foreach ($underling as $underling)
+                                            <option value="{{$underling->kodeAgen}}">{{$underling->nama}}</option>
+                                            @endforeach
                                             @endif
 
-                                            </x-shipment.input>
-                                        </div>
-                                        <button type="submit" class="btn btn-primary">Cari data</button>
+                                        </x-shipment.input>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Cari data</button>
                                 </div>
                             </form>
                         </div>
@@ -63,7 +72,8 @@
             </div>
             <div class="body">
                 <div class="table-responsive">
-                    <table id="{{ $tableClass}}" class="table table-bordered table-striped table-hover w-100 datatable-export-excel">
+                    <table id="{{ $tableClass }}"
+                        class="table table-bordered table-striped table-hover w-100 datatable-export-excel">
                         <thead>
                             <tr>
                                 <th>Tanggal</th>
@@ -76,17 +86,18 @@
                         </thead>
                         <tbody>
                             @if ($statusRes =='success')
-                                @foreach ($orderData as $xdata)
-                                    <tr>
-                                        <td>{{ $xdata['tglOrder'] }}</td>
-                                        <td>{{ $xdata['noresi'] }}</td>
-                                        <td>{{ $xdata['pengirim'] }} <br> {{ $xdata['telepon'] }}</td>
-                                        <td>{{ $xdata['penerima'] }} <br> {{ $xdata['teleponp'] }} <br> {{ $xdata['alamat'] }}</td>
-                                        <td>{{ $xdata['tujuan'] }}</td>
-                                        <td>Berat : {{ $xdata['berat'] }} <br> Qty : {{ $xdata['qty'] }}</td>
-                                        {{-- <td></td> --}}
-                                    </tr>
-                                @endforeach
+                            @foreach ($orderData as $xdata)
+                            <tr>
+                                <td>{{ $xdata['tglOrder'] }}</td>
+                                <td>{{ $xdata['noresi'] }}</td>
+                                <td>{{ $xdata['pengirim'] }} <br> {{ $xdata['telepon'] }}</td>
+                                <td>{{ $xdata['penerima'] }} <br> {{ $xdata['teleponp'] }} <br> {{ $xdata['alamat'] }}
+                                </td>
+                                <td>{{ $xdata['tujuan'] }}</td>
+                                <td>Berat : {{ $xdata['berat'] }} <br> Qty : {{ $xdata['qty'] }}</td>
+                                {{-- <td></td> --}}
+                            </tr>
+                            @endforeach
                             @endif
                         </tbody>
                     </table>
