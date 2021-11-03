4e8975d29753a59ef2c04f5a8d78854cf17420ba
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2021-11-04 00:36:05 +0700

    [ADD] check using two connection

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('faq','FaqController');
@@ -12,6 +14,13 @@ Route::apiResource('our-service','OurServiceController');
 Route::get('vision-mission', 'AboutUsController@getVisionMission');
 
 Route::get('previous-recipient/{id}', 'ShipmentOrderController@dummyPreviousRecipient');
+
+// Route::post('zipcode-list/{tableName}', function (Request $request, $tableName){
+
+//     $zipcode = DB::connection('mysql2')->table($tableName)->get();
+
+//     return response()->json($zipcode);
+// })->name('zipcode-list');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
