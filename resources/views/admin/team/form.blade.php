da0d7fadb3ef26f5580a47842498106e588a05b7
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2021-11-15 11:50:47 +0700

    [UPDATE] refactoring position list into manual input #2

--- resources/views/admin/team/form.blade.php
+++ resources/views/admin/team/form.blade.php
@@ -4,28 +4,22 @@
         @method('PUT')
     @endisset
 
-    <x-admin.input name="name" label="Nama member" 
-    value="{{ isset($data) ? $data->name : '' }}" 
-    class="not-allow-number" minlength="3" maxlength="60" required></x-admin.input>
-    
-    <x-admin.input type="select" label="Jabatan" 
-    name="{{ empty($data) ? 'position_id' : 'position_id_edit' }}" required>
-        @foreach ($positionList as $position)
-            <option value="{{ $position->id }}"
-            @if (isset($data) and ($position->id === $data->position_id) or
-            old('position_id') == $position->id) selected @endif>
-                {{ $position->name }}
-            </option>
-        @endforeach
-    </x-admin.input>
+    <x-admin.input name="name" label="Nama member"
+    value="{{ isset($data) ? $data->name : '' }}"
+    pattern="[a-zA-Z.\s]+" title="Nama tidak boleh mengandung angka atau karakter spesial"
+    minlength="3" maxlength="60" required></x-admin.input>
+
+    <x-admin.input label="Jabatan"
+    name="position" required />
 
     <x-admin.input type="textarea" label="Deskripsi member" name="short_desc"
-    minlength="8" maxlength="50" value="{{ $data->short_desc ?? '' }}" 
+    minlength="8" maxlength="50" value="{{ $data->short_desc ?? '' }}"
     rows="5" required/>
 
-    <x-admin.input label="{{ isset($data) ? 'Ubah' : 'Pilih' }} profile foto" 
-    type="file" name="{{ isset($data) ? 'avatar_edit' : 'avatar' }}" 
-    :is-required="isset($data) ? false : true" accept="image/*"/>
+    <x-admin.input label="{{ isset($data) ? 'Ubah' : 'Pilih' }} profile foto"
+    type="file" name="{{ isset($data) ? 'avatar_edit' : 'avatar' }}" accept="image/*">
+        <small>Atau kosongkan jika ingin menggunakan avatar default</small>
+    </x-admin.input>
 
     <button type="submit" class="btn btn-primary">Submit</button>
 </form>
